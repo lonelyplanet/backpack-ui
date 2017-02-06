cfb940ad5f669e026bab6f39880116fad7cf0cd7
Tom Cunningham <tom.cunningham@lonelyplanet.com>
2017-02-06 15:16:35 -0600

    Update Card

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,66 +1,62 @@
 import React, { PropTypes } from "react";
-import styled, { css } from "styled-components";
 import radium from "radium";
 import Heading from "../heading";
 import Price from "../price";
+import BulletDescription from "../bulletDescription";
+import TextBubble from "../textBubble";
+import CoverPhoto from "../coverPhoto";
 import { OuterLink, InnerLink } from "../nestedLink";
 import { iconFromString } from "../../utils/icon";
 import { rgba } from "../../utils/color";
-import { color, timing, typography, zIndex } from "../../../settings.json";
+import { color, timing, zIndex } from "../../../settings.json";
 
-const formatBulletList = (description) => [].concat(description).join(String.fromCharCode("8226"));
+// const baseHoverStyles = (fullBleed) => {
+//   if (fullBleed) {
+//     return css`
+//       .CardImageContainer {
+//         transition: transform ${timing.default} ease-in-out;
+//         transform: translateY(-2px);
+//       }
+//       .Heading {
+//         color: ${color.blue} !important;
+//       }
+//     `;
+//   }
+//
+//   return css`
+//     z-index: ${zIndex.top - 1};
+//     transform: perspective(1px) scale(1.03);
+//     box-shadow: 0px 28px 81px -7px ${rgba(color.black, 0.44)};
+//   `;
+// };
 
-const baseHoverStyles = (fullBleed) => {
-  if (fullBleed) {
-    return css`
-      .CardImageContainer {
-        transition: transform ${timing.default} ease-in-out;
-        transform: translateY(-2px);
-      }
-      .Heading {
-        color: ${color.blue} !important;
-      }
-    `;
-  }
-
-  return css`
-    z-index: ${zIndex.top - 1};
-    transform: perspective(1px) scale(1.03);
-    box-shadow: 0px 28px 81px -7px ${rgba(color.black, 0.44)};
-  `;
+const containerHoverStyles = {
+  boxShadow: `0 28px 81px -7px ${rgba(color.black, 0.44)}`,
+  transform: "perspective(1px) scale(1.03)",
+  zIndex: (zIndex.top - 1),
 };
 
 const styles = {
-  cardBase: {
+  container: {
+    boxShadow: `0 12px 34px 0 ${rgba(color.black, 0.11)}`,
     cursor: "pointer",
-    maxWidth: "409px",
+    maxWidth: "412px",
     transition: `box-shadow ${timing.default} ease-in-out,
       transform ${timing.default} ease-in-out`,
 
-    // &:hover {
-    //   ${({ theme }) => baseHoverStyles(theme.fullBleed)}
-    // }
+    ":hover": containerHoverStyles,
+    ":active": containerHoverStyles,
+    ":focus": containerHoverStyles,
   },
 
-  imageContainer: {
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    minHeight: "224px",
-    position: "relative",
+  coverPhoto: {
     transition: `transform ${timing.default} ease-in-out`,
   },
 
-  bubbleText: {
-    backgroundColor: rgba(color.black, 0.24),
-    borderRadius: "44px",
+  textBubble: {
     bottom: "8px",
-    color: color.white,
-    fontSize: "14px",
-    padding: "5px 15px",
     position: "absolute",
     right: "16px",
-    weight: typography.fontWeightMedium,
   },
 
   bottomLeft: {
@@ -106,7 +102,6 @@ const styles = {
   },
 
   cardContent: {
-    // padding: ${({ theme }) => (theme.fullBleed ? "32px 0 42px" : "32px 24px 42px")},
   },
 };
 
@@ -124,80 +119,96 @@ const Card = ({
   price,
   theme,
 }) => (
-  <OuterLink
+  <div
+    className="Card"
     style={[
-      styles.cardBase,
-      theme.minHeight && { minHeight: theme.minHeight },
-      !theme.fullBleed && { boxShadow: `0 12px 34px 0 ${rgba(color.black, 0.11)}` },
+      styles.container,
+      // theme.minHeight && { minHeight: theme.minHeight },
+      // !theme.fullBleed && { boxShadow: `0 12px 34px 0 ${rgba(color.black, 0.11)}` },
     ]}
-    href={link}
   >
-    <div
-      className="CardImageContainer"
-      style={[
-        styles.imageContainer,
-        { backgroundImage: `url(${image})` },
-      ]}
-    >
-      {imageIcon &&
-        <div
-          className="ImageIconContainer"
-          style={[
-            styles.bottomLeft,
-            styles.iconContainer,
-          ]}
-        >
-          {iconFromString(imageIcon, imageIconProps)}
-        </div>
-      }
+    <OuterLink href={link} element="a">
+      <div className="CardImageContainer">
+        {image &&
+          <CoverPhoto
+            src={image}
+            width={412}
+            height={232}
+            style={styles.coverPhoto}
+          />
+        }
 
-      {imageBubbleText &&
-        <div className="ImageBubbleText" style={styles.bubbleText}>
-          {imageBubbleText}
-        </div>
-      }
-    </div>
-
-    <div className="CardContent" style={styles.cardContent}>
-      <div className="CardBullets" style={styles.cardBullets}>
-        {formatBulletList(description)}
-        {descriptionIcon &&
-          <InnerLink
-            style={styles.descriptionIconContainer}
-            onClick={onDescriptionIconClick}
+        {imageIcon &&
+          <div
+            className="ImageIconContainer"
+            style={[
+              styles.bottomLeft,
+              styles.iconContainer,
+            ]}
           >
-            {iconFromString(descriptionIcon, descriptionIconProps)}
-          </InnerLink>
+            {iconFromString(imageIcon, imageIconProps)}
+          </div>
+        }
+
+        {imageBubbleText &&
+          <TextBubble style={styles.textBubble}>
+            {imageBubbleText}
+          </TextBubble>
         }
       </div>
 
-      <Heading
-        level={2}
-        weight="thin"
-        override={{ lineHeight: 1.33 }}
+      <div
+        className="CardContent"
+        style={[
+          styles.cardContent,
+          theme.fullBleed ? { padding: "32px 0 42px" } : { padding: "32px 24px 42px" },
+        ]}
       >
-        {title}
-      </Heading>
-
-      {price &&
-        <div
-          className="CardPrice-container"
-          style={styles.bottomLeft}
-        >
-          {price.sale &&
-            <span className="SalePrice" style={styles.salePrice}>
-              was <span style={styles.crossThrough}>${price.regular}</span>
-            </span>
+        <div className="CardBullets" style={styles.cardBullets}>
+          {description &&
+            <BulletDescription
+              description={description}
+            />
           }
 
-          <Price
-            amount={price.sale || price.regular}
-            emphasized
-          />
+          {descriptionIcon &&
+            <InnerLink
+              style={styles.descriptionIconContainer}
+              onClick={onDescriptionIconClick}
+            >
+              {iconFromString(descriptionIcon, descriptionIconProps)}
+            </InnerLink>
+          }
         </div>
-      }
-    </div>
-  </OuterLink>
+
+        <Heading
+          level={2}
+          weight="thin"
+          override={{ lineHeight: 1.33 }}
+        >
+          {title}
+        </Heading>
+
+        {price &&
+          <div
+            className="CardPrice-container"
+            style={styles.bottomLeft}
+          >
+            {price.sale &&
+              <span className="SalePrice" style={styles.salePrice}>
+                was <span style={styles.crossThrough}>${price.regular}</span>
+              </span>
+            }
+
+            <Price
+              amount={price.sale || price.regular}
+              emphasized
+            />
+          </div>
+        }
+      </div>
+    </OuterLink>
+  </div>
 );
 
 Card.propTypes = {
