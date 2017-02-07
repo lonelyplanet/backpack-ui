ea7f1d79d79f9e52a0c0be29fef4e187cdca9f5f
Tom Cunningham <tom.cunningham@lonelyplanet.com>
2017-02-07 12:12:38 -0600

    Update price card component

--- src/components/cardPrice/index.jsx
+++ src/components/cardPrice/index.jsx
@@ -1,17 +1,71 @@
-import React from "react";
-import { ThemeProvider } from "styled-components";
-import Card from "../card";
+import React, { PropTypes } from "react";
+import radium from "radium";
+import {
+  Card,
+  CardImage,
+  CardText,
+  CardDescription,
+  CardAnchor,
+} from "../card";
+import Price from "../price";
 
-const CardPrice = (props) => (
-  <ThemeProvider
-    theme={{
-      minHeight: "535px",
-    }}
+const CardPrice = ({
+  href,
+  imageSrc,
+  heading,
+  bullets,
+  price,
+  style,
+}) => (
+  <Card
+    className="CardPrice"
+    style={[style, { minHeight: "535px" }]}
   >
-    <Card
-      {...props}
+    <CardImage
+      href={href}
+      src={imageSrc}
     />
-  </ThemeProvider>
+
+    <CardText>
+      <CardAnchor
+        href={href}
+        style={{ minHeight: "303px" }}
+      >
+        <CardDescription
+          heading={heading}
+          bullets={bullets}
+        />
+
+        {price &&
+          <Price
+            amount={price.regular}
+            discountedAmount={price.sale}
+            style={{ bottom: "27px", position: "absolute" }}
+            parent="card"
+            emphasized
+          />
+        }
+      </CardAnchor>
+    </CardText>
+  </Card>
 );
 
-export default CardPrice;
+CardPrice.propTypes = {
+  href: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string.isRequired,
+  heading: PropTypes.string.isRequired,
+  bullets: PropTypes.arrayOf(PropTypes.string).isRequired,
+  price: PropTypes.shape({
+    sale: PropTypes.number,
+    regular: PropTypes.number,
+  }).isRequired,
+  style: PropTypes.objectOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.object,
+    ]),
+  ),
+};
+
+export default radium(CardPrice);
