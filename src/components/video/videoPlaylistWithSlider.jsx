018d577f2eb9f1d0c2a14320eaa8cc87901b9c5e
Chad Harter <harterc1@live.com>
2018-01-12 16:25:38 -0500

    fixed issue with VideoFeatured component hiding itself early

--- src/components/video/videoPlaylistWithSlider.jsx
+++ src/components/video/videoPlaylistWithSlider.jsx
@@ -73,9 +73,10 @@ class VideoPlaylistWithSlider extends React.Component {
   onLoadVideo = (video) => {
     this.setState({ video });
     if (!this.newPlaylist) {
+      console.log("this isn't a new playlist, so lets enable video info");
       this.setState({ enableVideoInfo: true });
     }
-
+    console.log("noting that this isn't a new playlist anymore");
     this.newPlaylist = false;
   }
 
@@ -99,6 +100,8 @@ class VideoPlaylistWithSlider extends React.Component {
     } = this.props;
 
     const { video, enableVideoInfo } = this.state;
+
+    console.log("rendering");
 
     return (
       <div className="VideoPlaylistWithSlider" style={style}>
